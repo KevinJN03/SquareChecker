97d2aa3dfead4b5882877eb3cef61486ea439cad
Kevin Jean <kevinjean321@gmail.com>
2023-03-06 09:47:11 +0000

    Add project files.

---
+++ SquareChecker.Tests/CheckerTests.cs
@@ -0,0 +1,27 @@
+﻿
+using Xunit;
+using SquareChecker;
+namespace SquareChecker.Tests
+{
+    
+    public class CheckerTests
+    {
+        [Theory]
+        [InlineData(-1, false)]
+        [InlineData(0, true)]
+        [InlineData(3, false)]
+        [InlineData(4, true)]
+        [InlineData(25, true)]
+        [InlineData(26, false)]
+        public void Check_IFReturnValue_IsTrueOrFalse(int number, bool expected)
+        {
+            //Arrange
+            var sut = new Checker();
+            //Act
+            var result = sut.Check(number);
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
---
+++ SquareChecker/Checker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareChecker
+{
+    public class Checker
+    {
+        public bool Check(int n)
+        {
+
+            if((n >= 0) && (Math.Sqrt(n) % 1 == 0)) {
+                
+                return true;
+            } else
+            { 
+                return false; 
+            }
+                
+           
+
+          
+
+
+        }
+    }
+}
